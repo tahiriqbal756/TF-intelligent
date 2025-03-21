87c61f40ffb6de069a35e6d17ad42697ac66df49
tahiriqbal756 <jackertfbhai@gmail.com>
2025-03-21 11:05:07 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ChatApp from "./ChatApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChatApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
